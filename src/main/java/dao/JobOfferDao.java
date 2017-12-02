06d080806bd48c238435756270ddc6a9a84a19c4
konrad1kurylo <konrad1kurylo@gmail.com>
2017-12-02 14:10:31 +0100

    dodano testy do jobOfferDao

--- src/main/java/dao/JobOfferDao.java
+++ src/main/java/dao/JobOfferDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import model.Employer;
 import model.JobOffer;
 import model.Location;
 import model.PayRange;
@@ -9,7 +10,7 @@ import java.util.List;
 public interface JobOfferDao
 {
     List<JobOffer> findByTitle(String title);
-    List<JobOffer> findByEmployer(String employer); //TODO
+    List<JobOffer> findByEmployer(Employer employer);
     List<JobOffer> findByPayRange(PayRange payRange);
     List<JobOffer> findByLocalization(Location localization);
 }
