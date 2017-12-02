d2d871b137eef71bb80fc278607f17057ee6ccb1
konrad1kurylo <konrad1kurylo@gmail.com>
2017-12-02 12:19:40 +0100

    dodane job offer oraz location ( helper do job offer)

---
+++ src/main/java/model/JobOffer.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+
+public class JobOffer
+{
+    private String title;
+    private String description;
+    private String jobType;
+    private String candidateRequirements;
+    private List<String> benefitList;
+    private Date offerDueDate;
+    private Date jobStartDate;
+    private String localization;
+    private List<String> jobConditions;
+}
