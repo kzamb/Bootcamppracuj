df16a60878148302f97434241a54a2ed109faad4
kzamb <kzamb@kablowka.net>
2017-12-03 14:57:38 +0100

    dodane LocationController+Dao

---
+++ src/main/java/pl/sages/dao/LocationDao.java
@@ -0,0 +1,10 @@
+package pl.sages.dao;
+
+import org.springframework.stereotype.Repository;
+import pl.sages.model.JobOffer;
+import pl.sages.model.Location;
+
+@Repository
+public interface LocationDao extends AbstractDao<Location> {
+
+}
--- src/main/java/pl/sages/model/Picture.java
+++ src/main/java/pl/sages/model/Picture.java
@@ -1,6 +1,7 @@
-package pl.sages.model.helpers;
+package pl.sages.model;
 
 import lombok.Data;
+import pl.sages.model.helpers.AbstractEntity;
 
 import javax.persistence.Entity;
 
